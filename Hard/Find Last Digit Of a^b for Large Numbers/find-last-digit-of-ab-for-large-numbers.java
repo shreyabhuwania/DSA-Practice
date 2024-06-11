ccd54e638228325a6520d0f2e299632d8aea6f91
shreyabhuwania <95756422+shreyabhuwania@users.noreply.github.com>
2024-06-12 02:56:04 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Find Last Digit Of a^b for Large Numbers/find-last-digit-of-ab-for-large-numbers.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S[] = read.readLine().split(" ");
+            
+            String a = S[0];
+            String b = S[1];
+
+            Solution ob = new Solution();
+            System.out.println(ob.getLastDigit(a,b));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int getLastDigit(String a, String b) {
+        // code here
+        if(b.equals("0"))
+        return 1;
+        if(a.equals("1"))
+        return 1;
+        
+        int exp;
+        int mod=modulo(4,b);
+        if(mod==0)
+            exp=4;
+        else
+            exp=mod;
+            
+        double result=Math.pow(a.charAt(a.length()-1)-'0',exp);
+        return (int) result%10;
+    }
+    static int modulo(int a,String b)
+    {
+        int res=0;
+        for(int i=0;i<b.length();i++)
+        res=(res*10+(b.charAt(i)-'0'))%a;
+        return res;
+    }
+}
